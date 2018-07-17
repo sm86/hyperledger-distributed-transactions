78dad23392fe8af1bf3f46e3938c634ec978592a
sm86 <shashank.motepalli@gmail.com>
2018-07-18 01:49:39 +0530

    mapping-Config file

--- destination/processTransaction.js
+++ destination/processTransaction.js
@@ -9,11 +9,19 @@ tx_db.getOldestTransaction(function(err, data) {
   
   processTransaction(data[0]);
 });
-
 function processTransaction(tx) {  
   console.log("Processing transaction")
-
-  invoke.invokeTransaction(tx, function(err) {  
+  var tx_base = tx.tx_object.payload.action.proposal_response_payload.extension.results.ns_rwset[0].rwset.writes[0];
+  tx_base.value = JSON.parse(tx_base.value);
+  
+  var transaction = {};
+  for(i =0; i<config.object.length;i++){
+      if(config.object[i].isConstant)
+          transaction[config.object[i].attribute] = config.object[i].value;
+      else
+          transaction[config.object[i].attribute] = eval(config.object[i].value);
+  }
+  invoke.invokeTransaction(transaction, function(err) {  
     if (err) {
       throw err;
     }
